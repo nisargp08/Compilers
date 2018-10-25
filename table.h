bda17130c8e2d7b272900b7956e30b26123cbe98
Nisarg Patel <d2mmrclimb@gmail.com>
2018-10-25 19:18:22 -0400

    Table.h without String literal

--- table.h
+++ table.h
@@ -24,6 +24,7 @@
 /*   Source end-of-file (SEOF) sentinel symbol
 *    '\0' or one of 255,0xFF,EOF
 */
+#define SEOF 255
 
 /*  Special case tokens processed separately one by one
 *  in the token-driven part of the scanner
@@ -33,43 +34,65 @@
 *  .AND., .OR. , SEOF, 'illegal symbol',
 */
 
-
-REPLACE *ESN* and *ESR* WITH YOUR ERROR STATE NUMBER
-#define ES  *ESN* /* Error state  with no retract */
-#define ER  *ESR* /* Error state  with retract */
-#define IS -1    /* Inavalid state */
+#define ES  11		/* Error state  with no retract */
+#define ER  12		/* Error state  with retract */
+#define IS -1		/* Inavalid state */
 
 /* State transition table definition */
 
-REPLACE *CN* WITH YOUR COLUMN NUMBER
+/* Table column is assigned with value 8 as we have six input symbols  including " and SEOF */
+#define TABLE_COLUMNS 8
 
-#define TABLE_COLUMNS *CN*
 /*transition table - type of states defined in separate table */
 int  st_table[][TABLE_COLUMNS] = {
-	/* State 0 */{ YOUR INITIALIZATION },
-	/* State 1 */{ YOUR INITIALIZATION },
-	.
-	.YOUR TABLE INITIALIZATION HERE
-	.
-	/* State N */  {YOUR INITIALIZATION},
+	/* State 0 */	{ 0 , 6 , 4 , ES , ES , ES , 9 , },
+	/* State 1 */	{ 1 , 1 , 1 , ES , 3 , 2 , 2 ,  },
+	/* State 2 */	{ IS , IS , IS , IS , IS , IS , IS , },
+	/* State 3 */	{ IS , IS , IS , IS , IS , IS , IS , },
+	/* State 4 */	{ ES , 4 , 4 , 7 , 5 , 5 , 5 ,  },
+	/* State 5 */	{ IS , IS , IS , IS , IS , IS , IS , },
+	/* State 6 */	{ ES , 0 , ES , 7 , ES , 5 , 5 , },
+	/* State 7 */	{ ES , 7 , 7 , ES , 8 , 8 , 8 , },
+	/* State 8 */	{ IS , IS , IS , IS , IS , IS , IS , },
+	/* State 9 */
+	/* State 10 */
+	/* State 11 */	{ IS , IS , IS , IS , IS , IS , IS , },
+	/* State 12 */	{ IS , IS , IS , IS , IS , IS , IS , },
+};
 
-	/* Accepting state table definition */
-	REPLACE *N1*, *N2*, and *N3* WITH YOUR NUMBERS
-#define ASWR     *N1*  /* accepting state with retract */
-#define ASNR     *N2*  /* accepting state with no retract */
-#define NOAS     *N3*  /* not accepting state */
+/* Accepting state table definition */
+#define ASWR     2  /* accepting state with retract */
+#define ASNR     1  /* accepting state with no retract */
+#define NOAS     0  /* not accepting state */
 
-	int as_table[] = { YOUR INITIALIZATION HERE - USE ASWR, ASNR, NOAS };
+	int as_table[] = { 
+		/* State 0 */	NOAS,
+		/* State 1 */	NOAS,
+		/* State 2 */	ASWR,
+		/* State 3 */	ASNR,
+		/* State 4 */	NOAS,
+		/* State 5 */	ASWR,
+		/* State 6 */	NOAS,
+		/* State 7 */	NOAS,
+		/* State 8 */	ASWR,
+		/* State 9 */	NOAS,
+		/* State 10 */	
+		/* State 11 */	ASNR,
+		/* State 12 */	ASWR
+	};
 
 /* Accepting action function declarations */
 
-FOR EACH OF YOUR ACCEPTING STATES YOU MUST PROVIDE
-ONE FUNCTION PROTOTYPE.THEY ALL RETURN Token AND TAKE
-ONE ARGUMENT : A string REPRESENTING A TOKEN LEXEME.
-
-Token aa_funcXX(char *lexeme);
-
-Replace XX with the number of the accepting state : 02, 03 and so on.
+/* Each accepting states 
+	Argument	:	Takes a string representing a token lexeme 
+	Return type :	Token
+*/
+Token aa_func02(char *lexeme);	/* VID AVID/KW */
+Token aa_func03(char *lexeme);	/* VID SVID */
+Token aa_func05(char *lexeme);	/* DIL */
+Token aa_func08(char *lexeme);	/* FPL */
+Token aa_func11(char *lexeme);	/* ES */
+Token aa_func12(char *lexeme);	/* ER */
 
 /* defining a new type: pointer to function (of one char * argument)
 returning Token
@@ -83,15 +106,21 @@ typedef Token(*PTR_AAF)(char *lexeme);
 * Token (*aa_table[])(char lexeme[]) = {
 */
 
+/* Array of pointers is initialized here */
 PTR_AAF aa_table[] = {
-
-
-	HERE YOU MUST PROVIDE AN INITIALIZATION FOR AN ARRAY OF POINTERS
-	TO ACCEPTING FUNCTIONS.THE ARRAY HAS THE SAME SIZE AS as_table[].
-	YOU MUST INITIALIZE THE ARRAY ELEMENTS WITH THE CORRESPONDING
-	ACCEPTING FUNCTIONS(FOR THE STATES MARKED AS ACCEPTING IN as_table[]).
-	THE REST OF THE ELEMENTS MUST BE SET TO NULL.
-
+	/* State 0 */	NULL,
+	/* State 1 */	NULL,
+	/* State 2 */	aa_func02,
+	/* State 3 */	aa_func03,
+	/* State 4 */	NULL,
+	/* State 5 */	aa_func05,
+	/* State 6 */	NULL,
+	/* State 7 */	NULL,
+	/* State 8 */	aa_func08,
+	/* State 9 */	NULL,
+	/* State 10 */
+	/* State 11 */	aa_func11,
+	/* State 12 */	aa_func12
 };
 
 /* Keyword lookup table (.AND. and .OR. are not keywords) */
